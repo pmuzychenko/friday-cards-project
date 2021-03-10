602b8914bd1a1ad343035ec7a7446a9c45a8b769
pmuzychenko <pmuzychenko@gmail.com>
2021-03-10 18:41:29 +0200

    feat: render userData using useSelector in the Profile component

--- src/components/common/pages/Profile.tsx
+++ src/components/common/pages/Profile.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../../../app/store";
+import {UserDataType} from "../../../app/login-reducer";
 
 export const Profile = () => {
+    const userProfileData = useSelector<AppRootStateType, UserDataType | null>(state => state.login.data)
     return (
         <div>
-            Profile Page will be here
+            <div>name: {userProfileData ? userProfileData.name: ''}</div>
         </div>
     );
 }
