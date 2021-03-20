b1dc0bdbcfa9213bd0d5ae973e5e291d98df55d8
pmuzychenko <pmuzychenko@gmail.com>
2021-03-20 11:46:38 +0200

    fix: delete private setting for adding pack in the post request

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,8 +3,8 @@ import { LoginFormData } from "../reducers/login-reducer";
 
 
 const instance = axios.create({
-    baseURL: `https://neko-back.herokuapp.com/2.0`,
-    // baseURL: `http://localhost:7542/2.0/`,
+    //baseURL: `https://neko-back.herokuapp.com/2.0`,
+    baseURL: `http://localhost:7542/2.0/`,
     withCredentials: true
 })
 
@@ -47,7 +47,7 @@ export const apiPacks = {
     },
     addPack() {
         return instance.post(`cards/pack`, {
-            cardsPack: { name: 'PROJECT-CARD', private: true }
+            cardsPack: { name: 'PROJECT-CARD'}
         })
     },
     deletePack(id: string) {
